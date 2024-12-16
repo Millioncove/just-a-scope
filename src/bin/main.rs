608975f9164fc592dc1b6c95289f234a0a33edb4
Emil Karlsson <emil.axel.karlsson@gmail.com>
2024-12-16 23:02:33 +0100

    TCP socket works! (barely)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,124 +1,137 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
+use core::net::Ipv4Addr;
+use alloc::boxed::Box;
+
+use embassy_net::{tcp::TcpSocket, Config, IpListenEndpoint, Ipv4Cidr, Runner, Stack, StackResources, StaticConfigV4};
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::delay::Delay;
-use esp_hal::gpio::{Level, Output};
-use esp_hal::peripherals::Peripherals;
-use esp_hal::time::{now, Instant};
-use esp_hal::prelude::*;
-use esp_hal::rng::Rng;
-use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{prelude::*,delay::Delay, gpio::{Level,Output}, peripherals::Peripherals, rng::Rng, timer::timg::TimerGroup};
 use esp_println::println;
-use log::info;
-use esp_wifi::{self, wifi::{WifiController,Configuration,AccessPointConfiguration, utils::create_ap_sta_network_interface}};
-use smoltcp::iface::{SocketSet, SocketStorage};
-use smoltcp::socket::udp::{PacketBuffer, PacketMetadata};
-use smoltcp::wire::{IpCidr, Ipv4Address, Ipv4Cidr, UdpPacket};
-use smoltcp_nal::embedded_nal::{UdpClientStack, UdpFullStack};
-use smoltcp_nal::NetworkStack;
-use embedded_time::rate::Fraction;
+use esp_wifi::wifi::{WifiApDevice, WifiDevice};
+use esp_wifi::{self, wifi::{Configuration,AccessPointConfiguration}};
+use esp_hal_dhcp_server::{structs::DhcpServerConfig, simple_leaser::SingleDhcpLeaser};
 
-struct ScuffedClock {
-    pub now: fn() -> Instant
+// Get it? Because it's just running all the time :D
+#[embassy_executor::task]
+async fn marathon (mut runner: Runner<'static, WifiDevice<'static, WifiApDevice>>) {
+    runner.run().await
 }
 
-const ONE_SECOND_IN_TICKS: u32 = esp_hal::time::Duration::secs(1).ticks() as u32;
-impl embedded_time::Clock for ScuffedClock {
-    type T = u32;
-    
-    const SCALING_FACTOR: Fraction = Fraction::new(1, ONE_SECOND_IN_TICKS); // knas
+#[embassy_executor::task]
+async fn dhcp_server(stack: Stack<'static>) {
+    let config = DhcpServerConfig {
+        ip: Ipv4Addr::new(192, 168, 1, 1),
+        lease_time: Duration::from_secs(1800),
+        gateways: &[],
+        subnet: None,
+        dns: &[]
+    };
 
-    fn try_now(&self) -> Result<embedded_time::Instant<Self>, embedded_time::clock::Error> {
-        Ok(embedded_time::Instant::new((self.now)().ticks() as u32))
-    }
+    let mut leaser = SingleDhcpLeaser::new(Ipv4Addr::new(192, 168, 1, 111));
+
+    esp_hal_dhcp_server::run_dhcp_server(stack, config, &mut leaser).await;
 }
 
-#[entry]
-fn main() -> ! {
+#[embassy_executor::task]
+async fn tcp_test(stack: Stack<'static>) {
+    let mut tcp_rx_buffer = [0u8;500];
+    let mut tcp_tx_buffer = [0u8;500];
+    let mut tcp_socket: TcpSocket<'_> = TcpSocket::new(stack, &mut tcp_rx_buffer, &mut tcp_tx_buffer);
+    tcp_socket.accept(IpListenEndpoint{addr: None, port: 80}).await.expect("Failed to start listening on tcp port.");
+
+    let mut buf = [0; 1024];
+        loop {
+            let size_received = tcp_socket.read(&mut buf).await;
+            match size_received {
+                Ok(0) => panic!("Our tcp socket was closed while we expected to read from it!"),
+                Ok(n) => println!("received {n} from! I have no clue where from!"),
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+}
+
+#[main]
+async fn main(spawner: embassy_executor::Spawner) -> ! {
+    // Initialize heap, logger, and peripherals.
     esp_alloc::heap_allocator!(72 * 1024);
-
     esp_println::logger::init_logger_from_env();
     let peripherals: Peripherals = esp_hal::init({
         let mut config: esp_hal::Config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::max();
         config
     });
+    
+    // Initialize embassy so async works at all.
+    let timg1 = TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timg1.timer0);
 
+    // Network stack setup.
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let esp_wifi_controller = esp_wifi::init(timg0.timer0, Rng::new(peripherals.RNG), peripherals.RADIO_CLK).unwrap();
+    let esp_wifi_controller: Box<esp_wifi::EspWifiController<'static>> = Box::new(esp_wifi::init(
+        timg0.timer0, 
+        Rng::new(peripherals.RNG), 
+        peripherals.RADIO_CLK
+    ).unwrap());
+    let esp_wifi_controller = Box::leak(esp_wifi_controller);
+    let (device, mut controller) = esp_wifi::wifi::new_with_mode(esp_wifi_controller, peripherals.WIFI, WifiApDevice).unwrap();
+    let stack_conf = Config::ipv4_static(StaticConfigV4 {
+        address: Ipv4Cidr::new(Ipv4Addr::new(192, 168, 1, 1), 24),
+        gateway: Some(Ipv4Addr::new(192, 168, 1, 1)),
+        dns_servers: Default::default()
+    });
+    
+    let boxed_resources = Box::new(StackResources::<3>::new());
+    let leaked_resources = Box::leak(boxed_resources);
+    let random_seed = 687486766; // Extremely secure!
+    let (stack, runner) = embassy_net::new(device, stack_conf, leaked_resources, random_seed);
     
     
-    let interface = create_ap_sta_network_interface(&esp_wifi_controller, peripherals.WIFI).unwrap();
-    let mut controller: WifiController<'_> = interface.controller;
+    
 
+
+    // Access point configuration and startup.
     let ap_conf: AccessPointConfiguration = AccessPointConfiguration {
-        ssid: heapless::String::<32>::try_from("skogaholmslimpa").unwrap(),
+        ssid: heapless::String::<32>::try_from("jultomten").unwrap(),
         auth_method: esp_wifi::wifi::AuthMethod::WPA,
-        password: heapless::String::<64>::try_from("billigost").unwrap(),
+        password: heapless::String::<64>::try_from("skorsten").unwrap(),
         ..AccessPointConfiguration::default()
     };
-    
+    println!("Starting the wifi access point!");
     controller.set_configuration(&Configuration::AccessPoint(ap_conf)).expect("Failed to set the wifi configuration...");
     controller.start().expect("Welp, could not start the wifi controller...");
 
-    
-    // STACK
-    let device = interface.ap_device;
-    let interface = interface.ap_interface;
-    
-    // Sockets
-    let mut socket_storage: [SocketStorage;3] = Default::default();
-    let mut socket_set: SocketSet = SocketSet::new(&mut socket_storage[..]);
-    let mut rx_meta_storage: [PacketMetadata; 500] = [PacketMetadata::EMPTY; 500];
-    let mut rx_payload_storage: [u8; 1337] = [44; 1337];
-    let rx_buffer: PacketBuffer = PacketBuffer::new(&mut rx_meta_storage[..], &mut rx_payload_storage[..]);
-    let mut tx_meta_storage: [PacketMetadata; 500] = [PacketMetadata::EMPTY; 500];
-    let mut tx_payload_storage: [u8; 1337] = [44; 1337];
-    let tx_buffer= PacketBuffer::new(&mut tx_meta_storage[..], &mut tx_payload_storage[..]);
-    socket_set.add(smoltcp::socket::udp::Socket::new(rx_buffer, tx_buffer));
+    // Start running the stack concurrently.
+    spawner.spawn(marathon(runner)).expect("Could not start stack runner task.");
 
-    let bad_clock: ScuffedClock = ScuffedClock { now };
-    let mut stack = NetworkStack::new(interface, device, socket_set, bad_clock);
-
-    let best_ipv4_address: Ipv4Cidr = Ipv4Cidr::new(Ipv4Address::new(69, 69, 69, 69), 24);
-    stack.interface_mut().update_ip_addrs(|v|  v.push(IpCidr::Ipv4(best_ipv4_address)).expect("Could not add IP address."));
-    let mut udp_sock = stack.socket().expect("Our puny attempt at making a UDP socket failed :/");
-    stack.bind(&mut udp_sock, 67).expect("Could not bind the UDP socket to port 67.");
+    // Block until the stack is ready.
+    loop {
+        if stack.is_link_up() {
+            break;
+        }
+        embassy_time::Timer::after(embassy_time::Duration::from_millis(500)).await;
+    }
     
+    println!("Starting dhcp server!");
+    let s = stack.clone();
+    spawner.spawn(dhcp_server(stack)).expect("Could not spawn dhcp server task.");
 
+    println!("Starting tcp testing!");
+    spawner.spawn(tcp_test(stack)).expect("Could not spawn tcp test task.");
+
+    Timer::after(Duration::from_secs(120)).await;
+    println!("Closing dhcp server after 2m...");
+    esp_hal_dhcp_server::dhcp_close();
+
+    // Blinky.
     let mut led: Output = Output::new(peripherals.GPIO21, Level::Low);
     let delay: Delay = Delay::new();
     println!("The setup didn't crash! Starting main loop...");
     loop {
-        let poll_result = stack.poll().expect("Even polling the stack failed!");
-
-        if poll_result {
-            let mut buffer: [u8; 500] = [0; 500];
-            let potential_datagram = stack.receive(&mut udp_sock, &mut buffer);
-            match potential_datagram {
-                Ok((size, remote)) => {
-                    println!("Datagram of size {size} from {remote}! Data: ",);
-                    println!(" {:x?}", buffer);
-                    //println!(" {}", str::from_utf8(&buffer).expect("Data cannot be converted to ascii :("));
-                    // let mut datagram = UdpPacket::new_checked(buffer).expect("Got a datagram that was seemingly not UDP");
-                    // println!("{:?}", datagram.payload_mut());
-                },
-                _ => ()
-            }
-            
-        }
-
         delay.delay(1000.millis());
         led.toggle();
-    }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
-}
-
-fn _print_access_points(controller: &mut WifiController<'_>) {
-    let found_in_scan = controller.scan_n::<55>().expect("Scan for access points failed.");
-    for ap in found_in_scan.0 {
-        info!("Found: {} | Signal strength: {}", ap.ssid, ap.signal_strength);
     }
 }
