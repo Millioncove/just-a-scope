1e066903b0f8d7358e40850ac54357be8ada9fc8
Emil Karlsson <emil.axel.karlsson@gmail.com>
2024-12-25 19:08:41 +0100

    Properly set up workspace.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -41,31 +41,7 @@ use heapless::String;
 use websocket_logistics::{send_message, OscilliscopePoint};
 use zerocopy::IntoBytes;
 
-mod websocket_logistics {
-    use embedded_io_async::{ErrorType, Write};
-    use zerocopy::{FromBytes, Immutable, IntoBytes};
-
-    #[derive(IntoBytes, FromBytes, Immutable)]
-    #[repr(C)]
-    pub struct OscilliscopePoint {
-        pub voltage: f64,
-        pub second: f64,
-    }
-
-    pub async fn send_message<W>(to: &mut W, data: &[u8]) -> Result<(), <W as ErrorType>::Error>
-    where
-        W: Write,
-    {
-        let fin_rsv_opcode = 0b10000010u8; // FIN and binary data.
-        let payload_length = data.len() as u8;
-        if payload_length > 126 {
-            panic!("Max payload length for a simple WebSocket message is 126 bytes.")
-        }
-        let header = [fin_rsv_opcode, payload_length];
-
-        to.write_all(&[&header, data].concat()).await
-    }
-}
+mod websocket_logistics;
 
 const CONNECTION_TIMEOUT_MS: u32 = 30_000;
 const WEBSOCKET_PORT: u16 = 43822;
