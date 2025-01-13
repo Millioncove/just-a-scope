04d5411388cbfa10dfebf47f78c5575e42be20d3
Emil Karlsson <emil.axel.karlsson@gmail.com>
2025-01-13 16:45:35 +0100

    Adjustments for voltage from measurement circuit.

--- src/bin/measure.rs
+++ src/bin/measure.rs
@@ -11,6 +11,8 @@ use esp_hal::{
 };
 
 const REFERENCE_VOLTAGE: f64 = 3.1f64;
+const MAX_VOLTAGE: f64 = 200f64;
+const PROBE_DISCONNECTED_VOLTAGE: f64 = 1.1945787545787547f64;
 
 pub fn measuring_task<const L: usize, const PIN: u8>(
     adc_peripheral: ADC1,
@@ -39,23 +41,19 @@ where
 
     loop {
         let current_second: f64 = now().ticks() as f64 / 1_000_000f64;
-        let voltage = nb::block!(adc.read_oneshot(&mut pin)).unwrap();
-        let voltage = voltage as f64 * REFERENCE_VOLTAGE / 4095f64;
+        let raw_adc_output = nb::block!(adc.read_oneshot(&mut pin)).unwrap();
+        let raw_adc_voltage = raw_adc_output as f64 * REFERENCE_VOLTAGE / 4095f64;
+        let adjusted_voltage =
+            (raw_adc_voltage - PROBE_DISCONNECTED_VOLTAGE) * MAX_VOLTAGE / REFERENCE_VOLTAGE;
         let new_point = OscilliscopePoint {
-            voltage,
+            voltage: adjusted_voltage,
             second: current_second,
         };
 
         let time_difference = last.second - before_last.second;
 
         if time_difference > 1f64
-            || !is_middle_point_removable_complicated(
-                &before_last,
-                &last,
-                &new_point,
-                0.1f64,
-                0.3f64,
-            )
+            || !is_middle_point_removable_complicated(&before_last, &last, &new_point, 0.3f64, 1f64)
         {
             match point_buffer_writer.append(last.clone()) {
                 Ok(_) => (),
